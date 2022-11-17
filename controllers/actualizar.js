15fb1f0c288c45f3a27bc615773f63f7a828e31b
DanielG <dannyelgomezz.13@gmail.com>
2022-11-17 16:33:46 -0500

    Modulo 5: Manejando Try, catch: errores

--- controllers/actualizar.js
+++ controllers/actualizar.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector('[data-form]');
 
-const obtenerInformacion = async() => {
+const obtenerInformacion = async () => {
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
 
@@ -11,9 +11,19 @@ const obtenerInformacion = async() => {
     }
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email");
-    const perfil = await  clientServices.detalleCliente(id)
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
+
+    try {
+        const perfil = await clientServices.detalleCliente(id)
+        if (perfil.nombre && perfil.email) {
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        }else{
+            throw new Error();
+        }
+
+    } catch (error) {
+        window.location.href = '/screens/error.html';
+    }
 };
 
 obtenerInformacion();
@@ -24,8 +34,8 @@ formulario.addEventListener("submit", (event) => {
     const id = url.searchParams.get("id");
     const nombre = document.querySelector("[data-nombre]").value;
     const email = document.querySelector("[data-email").value;
-    console.log(nombre,email);
-    clientServices.actualizarCliente(nombre,email,id).then(() => {
+    console.log(nombre, email);
+    clientServices.actualizarCliente(nombre, email, id).then(() => {
         window.location.href = '/screens/edicion_concluida.html'
     });
 })
