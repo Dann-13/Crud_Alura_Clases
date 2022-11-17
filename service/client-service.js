334741b33334fc42bada1c5520fa2802b565604d
DanielG <dannyelgomezz.13@gmail.com>
2022-11-17 13:54:00 -0500

    Modulo 3: Creando un cliente

--- service/client-service.js
+++ service/client-service.js
@@ -1,8 +1,18 @@
 //este archivo solo haremos la comunicacion con el servidor
 const lista_cliente = () => fetch("http://localhost:3000/perfil").then( response => response.json());
+const crearCliente = (nombre,email) => {
+    return fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
 
+        },
+        body: JSON.stringify({nombre,email,id:uuid.v4()})
+    })
+};
 export const clientServices = {
-    lista_cliente
+    lista_cliente,
+    crearCliente
 }
 
 
